c2c2d2d688afc069d1a0e77a0c72f4fbd4dfdbc2
noufjafari <engnoufjafari@gmail.com>
2023-09-11 13:32:15 +0300

    changes

--- Finel/src/Components/contexts/AddExpense.tsx
+++ Finel/src/Components/contexts/AddExpense.tsx
@@ -34,7 +34,7 @@ export default function AddExpense({
           className="flex justify-center fixed bg-black/25 top-0 left-0 right-0 z-50  w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-screen max-h-full"
         >
           <div className=" relative w-full max-w-2xl max-h-full">
-            <div className="mt-40 relative bg-white rounded-lg shadow dark:bg-slate-300">
+            <div className="mt-40 relative bg-white rounded-lg shadow ">
               <div className="flex items-start justify-between p-4 rounded-t ">
                 <h3 className="m-5 text-3xl font-semibold ml-60 text-gray-400 ">
                   New Expense
@@ -106,7 +106,7 @@ export default function AddExpense({
                   </select>
                 </div>
 
-                <div className="flex items-center p-6 space-x-2 border-gray-200 rounded-b dark:border-gray-600">
+                <div className="flex items-center p-6 space-x-2 border-gray-200 rounded-b ">
                   <button
                     data-modal-hide="defaultModal"
                     type="submit"
